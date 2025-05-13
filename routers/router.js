65e3342bb6ec1e32627212cfa8ffb6b812aca31a
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-05-13 13:15:21 +0200

    fix images folder

--- routers/router.js
+++ routers/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { posts } = require("../posts/posts");
+const { posts } = require("../data/posts");
 // INDEX
 router.get("/", (req, res) => {
   res.json({
